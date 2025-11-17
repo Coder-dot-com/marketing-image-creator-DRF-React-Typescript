e3d511319c19481e8a8825d8f15ac315acfb5f34
PC Usman <usman17.s@hotmail.com>
2025-11-17 19:09:30 +0000

    user register app, view and url

--- backend/marketing_image_creator/settings.py
+++ backend/marketing_image_creator/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'imagecreator',
+    'users',
 
     'rest_framework',
     'corsheaders',
--- backend/marketing_image_creator/urls.py
+++ backend/marketing_image_creator/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
 
     path("api/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    
+    path("api/auth/", include("users.urls")),
 
     path('api/', include('imagecreator.urls')),
 ]
